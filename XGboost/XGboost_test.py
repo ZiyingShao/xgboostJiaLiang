282718f51c3e062704d003544c76d79162e25316
Shao Ziying <shaoziying@outlook.com>
2021-08-05 17:31:13 +0800

    得到残差

--- XGboost/XGboost_test.py
+++ XGboost/XGboost_test.py
@@ -35,16 +35,16 @@ def get_train_val_test_data(df, train_pct=0.8, train_start_date=None, train_end_
     '''
     参数：train_pct:在训练集中作为验证集的比例
     '''
-    train_val_df = df[(df['trade_date'] >= train_start_date) & (df['trade_date'] <= train_end_date)]
+    train_val_df = df[(df['date'] >= train_start_date) & (df['date'] <= train_end_date)]
     train_val_df = train_val_df.sample(frac=1, random_state=30).reset_index(drop=True)
 
     #拆分训练集，验证集
     train_df = train_val_df.iloc[0:int(len(train_val_df)*train_pct)]
     val_df = train_val_df.iloc[int(len(train_val_df)*train_pct):]
 
-    test_df = df[df['trade_date'] > train_end_date]
+    test_df = df[df['date'] > train_end_date]
     if test_start_date and test_end_date:
-        test_df = test_df[(test_df['trade_date']>= test_start_date ) & (test_df['trade_date'] <= test_end_date)]
+        test_df = test_df[(test_df['date']>= test_start_date ) & (test_df['date'] <= test_end_date)]
     print("[data verify], train, %s to %s" % (min(train_df.trade_date.values), max(train_df.trade_date.values)))
     print("[data verify], val, %s to %s" % (min(val_df.trade_date.values), max(val_df.trade_date.values)))
     print("[data verify], test, %s to %s" % (min(test_df.trade_date.values), max(test_df.trade_date.values)))
@@ -62,6 +62,7 @@ def predict_factors(train_df, val_df, test_df, factor_names):
 with open(r'F:\WorkplaceSzy\ipcamaster\all_stocks.pkl', 'rb') as f:
     all_stocks = pickle.load(f)
 
+x_barra_coef = pd.read_csv(r'F:\WorkplaceSzy\XGboost\coef_barra(用于计算残差).csv')
 #得到时间频率列表和Barra因子
 x_barra = get_barra(all_stocks, '2010-03-01', '2021-01-04')
 frequency = get_trading_day_list('2010-03-01', '2021-01-04', frequency='month')
@@ -71,13 +72,14 @@ x_barra = x_barra[list(x_barra['date'].apply(lambda x: (x in frequency)))]
 ID = dict(zip(np.unique(x_barra.code).tolist(), np.arange(1, len(np.unique(x_barra.code)) + 1)))
 x_barra.code = x_barra.code.apply(lambda x: ID[x])
 
+x_barra.to_csv(r'F:\WorkplaceSzy\XGboost\barra值.csv')
 #standardize Barra
 valuation_data = get_valuation(all_stocks, start_date='2010-03-01', end_date='2021-01-04', count=None)
 market_cap = pd.DataFrame(valuation_data, columns=['date','market_cap'])
 market_cap['date'] = market_cap['date'].apply(lambda x: str(x))
 market_cap = market_cap[list(market_cap['date'].apply(lambda x: (x in frequency)))]
-barra_mean = np.array(x_barra.iloc[:, 2:]) * np.array(market_cap.iloc[:, 1:2])
 
+barra_mean = np.array(x_barra.iloc[:, 2:]) * np.array(market_cap.iloc[:, 1:2])#Barra因子乘market_cap
 bs = [] #barra mean for each barra factor
 for b in range(0, np.shape(barra_mean)[1]):
     bi = np.array(barra_mean[:, b]).sum()
@@ -85,48 +87,77 @@ for b in range(0, np.shape(barra_mean)[1]):
     bs.append(b_cap)
 
 n = np.shape(np.array(x_barra.iloc[:, 2:]))[0]
-
-barra_standardized= []
-for i in range(0, np.shape(np.array(x_barra.iloc[:,2:]))[1]):
-    diff = np.array(x_barra.iloc[:, 2:3+i]) - np.array(bs[i])
+m = np.shape(np.array(x_barra.iloc[:, 2:]))[1]
+barra_standardized = []
+for i in range(0, m):
+    diff = np.array(x_barra.iloc[:, 2+i:3+i]) - np.array(bs[i])
     sqr_diff_sum = np.sum(np.square(diff))
     std = np.sqrt(sqr_diff_sum/n)
     bfs = []
     for j in range(0, n):
-        nominator = np.array(x_barra.iloc[:, 2:3+i])[:, j] - np.array(bs[i])
+        nominator = np.array(x_barra.iloc[:, 2+i:3+i])[j, :] - np.array(bs[i])
         b_stand = nominator/std
         bfs.append(b_stand)
     barra_standardized.append(bfs)
 
-#def standardize_barra():
+d_test = np.array(barra_standardized)
+barra_s_v = d_test.reshape(11004, 10)
+barra_s_frame = pd.DataFrame(barra_s_v, columns=x_barra.columns[2:].values)
+code_frame = pd.DataFrame(x_barra.iloc[:, 0:2])
 
+b_s_test = np.append(np.array(code_frame), (np.array(barra_s_frame)), axis=1)
 
-
-
+b_s_frame = pd.DataFrame(b_s_test, columns=x_barra.columns.values)
+b_s_frame.to_csv(r'F:\WorkplaceSzy\XGboost\特殊标准化后的Barra.csv')
 #计算残差
-x_barra_coef = pd.read_csv(r'F:\WorkplaceSzy\XGboost\coef_barra(用于计算残差).csv')
 
 def aggfunc_return(barra, x_barra_coef):
     N = len(np.unique(barra.code))
     barra_return = []
     for n in range(1, N+1):
-        i_stock_barras = []
+        #i_stock_barras = []
         for t in range(0, 131):
-            i = (np.array(barra.iloc[0+(131*(n-1)):131 * n, 2:]) * np.array(x_barra_coef.iloc[1:11, 1:]).T + np.array(x_barra_coef.iloc[0:1, 1:]).T)[t].sum()
-            i_stock_barras.append(i)
-        barra_return.append(np.array(i_stock_barras))
+            i = (np.array(barra.iloc[0+(131*(n-1)):(131 * n), 2:]) * np.array(x_barra_coef.iloc[1:11, 1:]).T + np.array(x_barra_coef.iloc[0:1, 1:]).T)[t].sum()
+            barra_return.append(i)
     return barra_return
 
-cal_barra = aggfunc_return(x_barra, x_barra_coef.copy())
+cal_barra = aggfunc_return(b_s_frame, x_barra_coef.copy())
 cal_barra = np.array(cal_barra)
-barra_result = pd.DataFrame(cal_barra, columns=x_barra_coef.columns[1:].values, index=np.unique(all_stocks).tolist())
+cal_barra_test = np.append(np.array(x_barra['date'][0:11004]).reshape(11004, 1), np.array(cal_barra).reshape(11004,1), axis=1)
+barra_result = pd.DataFrame(cal_barra_test, columns={'date', 'barra price'})
+barra_result.to_csv(r'F:\WorkplaceSzy\XGboost\Barra标准化之后求到收盘价.csv')
 
+close_price_base = get_price(all_stocks, '2010-03-01', '2021-01-04')
+close_price_base['date'] = close_price_base['date'].apply(lambda x: str(x)[:10])
+close_price_base = close_price_base[list(close_price_base['date'].apply(lambda x: (x in frequency)))]
+
+c = np.array(close_price_base['close'])
+close_price_ratio = []
+for t in range(0, np.shape(np.array(close_price_base['close']))[0]-1):
+    ci = c[t+1]/c[t]
+    close_price_ratio.append(ci)
+
+c_test = np.append(np.array(close_price_base['date'][0:11003]).reshape(11003, 1), np.array(close_price_ratio).reshape(11003, 1), axis=1)
+close_price_ratio = pd.DataFrame(c_test, columns={'date', 'barra price'})
+close_price_ratio.to_csv(r'F:\WorkplaceSzy\XGboost\收盘价.csv')
+#得到残差
+a_residual = np.array(close_price_ratio.iloc[:, 1:2] - barra_result.iloc[:11003, 1:2])
+a_residual_t = np.append(np.array(close_price_base['date'][0:11003]).reshape(11003, 1), np.array(a_residual).reshape(11003, 1), axis=1)
+barra_residual = pd.DataFrame(a_residual_t)
+barra_residual_r = barra_residual.rename(columns={barra_residual.columns[0]: 'date', barra_residual.columns[1]: 'residual'})
+barra_residual_r.to_csv(r'F:\WorkplaceSzy\XGboost\residual.csv')
 #def barra_each_stock_return(x_barra, x_barra_coef):
     #for t in range(0, 131):
        # barra_return = x_barra.iloc[t,2:].values* x_barra_coef.iloc[1:,t+1].values + x_barra_coef.iloc[0:1,t+1].values
     #return barra_return
 
 #x_barra_test = pd.pivot_table(x_barra.copy(), index = ['date'], columns =['code'], values=x_barra.copy().columns.drop(['code','date'])) #aggfunc=barra_each_stock_return(x_barra.copy(), x_barra_coef.copy()))
+
+#读取截距得到训练集，测试集
+
+
+
+
 
 #读取截距数据
 residual = pd.read_csv(r'F:\WorkplaceSzy\XGboost\残差.csv')
